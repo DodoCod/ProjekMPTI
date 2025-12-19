db0b1df91b889a41a9456d47c5e92fcb7035cc24
Dodo Code <hoseafernando5072@gmail.com>
2025-12-19 21:00:55 +0700

    coba coba

--- app/Mail/TestResultMail.php
+++ app/Mail/TestResultMail.php
@@ -6,36 +6,41 @@ use App\Models\Participant;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Support\Facades\Request;
 
 class TestResultMail extends Mailable
 {
-    use Queueable, SerializesModels;
-
-    public $participant;
-
-    public function __construct(Participant $participant)
+    public function sendResultEmail(Request $request, Participant $participant)
     {
-        $this->participant = $participant;
+        return back()->with('info', 'Fitur email sementara dimatikan di production.');
     }
+    // use Queueable, SerializesModels;
 
-    public function envelope(): Envelope
-    {
-        return new Envelope(
-            subject: 'Hasil Skrining Kesehatan Mental DASS-42 Anda',
-        );
-    }
+    // public $participant;
 
-    public function content(): Content
-    {
-        return new Content(
-            // Merujuk ke resources/views/emails/test-result.blade.php
-            markdown: 'emails.test-result', 
-            with: [
-                'name' => $this->participant->name,
-                'result' => $this->participant->result,
-            ],
-        );
-    }
+    // public function __construct(Participant $participant)
+    // {
+    //     $this->participant = $participant;
+    // }
+
+    // public function envelope(): Envelope
+    // {
+    //     return new Envelope(
+    //         subject: 'Hasil Skrining Kesehatan Mental DASS-42 Anda',
+    //     );
+    // }
+
+    // public function content(): Content
+    // {
+    //     return new Content(
+    //         // Merujuk ke resources/views/emails/test-result.blade.php
+    //         markdown: 'emails.test-result', 
+    //         with: [
+    //             'name' => $this->participant->name,
+    //             'result' => $this->participant->result,
+    //         ],
+    //     );
+    // }
 }
